d59ed7f9822dd79529b13ce0c3a21b75f1ea0b70
Duong Anh Hao <haor06480@gmail.com>
2026-02-05 17:45:43 +0700

    nộp bài lab 6

--- bai4.c
+++ bai4.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Dương Anh Hào]
+ * MSSV:      [PS44305]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
@@ -9,4 +9,36 @@
 //  Output: Xuất ra màn hình ma trận bình phương 
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
 
+int main() {
+    int n, m;
+    int a[100][100];
+    int i, j;
+
+    // Nhập số hàng và số cột
+    printf("Nhap so hang n: ");
+    scanf("%d", &n);
+
+    printf("Nhap so cot m: ");
+    scanf("%d", &m);
+
+    // Nhập ma trận
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("Nhap a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Xuất ma trận bình phương
+    printf("Ma tran binh phuong:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d ", a[i][j] * a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
